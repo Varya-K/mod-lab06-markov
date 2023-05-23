082fb9363d91e0eb2150c9ace9411c36ebad1782
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-05-23 08:16:36 +0300

    Add files via upload

---
+++ include/textgen.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+class Textgen
+{
+public:
+	typedef deque<string> prefix;
+	Textgen(int prefix_size, string file_name);
+	string getSuffix(prefix pref);
+	string Gentext(int count_of_words);
+	map<prefix, vector<string>> GetStateTab();
+
+private:
+	map<prefix, vector<string>> statetab;
+	int npref;
+	prefix first_prefix;
+};
+
+
