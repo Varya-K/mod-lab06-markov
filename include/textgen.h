8468e32575c06e531ca3405b882dc5730b9d46fc
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-05-23 08:23:29 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -10,16 +10,14 @@ using namespace std;
 class Textgen
 {
 public:
-	typedef deque<string> prefix;
-	Textgen(int prefix_size, string file_name);
-	string getSuffix(prefix pref);
-	string Gentext(int count_of_words);
-	map<prefix, vector<string>> GetStateTab();
+    typedef deque<string> prefix;
+    Textgen(int prefix_size, string file_name);
+    string getSuffix(prefix pref);
+    string Gentext(int count_of_words);
+    map<prefix, vector<string>> GetStateTab();
 
 private:
-	map<prefix, vector<string>> statetab;
-	int npref;
-	prefix first_prefix;
+    map<prefix, vector<string>> statetab;
+    int npref;
+    prefix first_prefix;
 };
-
-
