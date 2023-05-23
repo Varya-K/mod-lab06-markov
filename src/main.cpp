a14452b3d2925bc8956606c6c7b50e1d1cd7df60
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-05-23 12:37:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include "textgen.h"
 
-int main()
-{
+int main() {
     const int NPREF = 2;
     const int MAXGEN = 1000;
     Textgen g = Textgen(NPREF, "Text2.txt");
     cout << g.Gentext(MAXGEN);
-    
 }
